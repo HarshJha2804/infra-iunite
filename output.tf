c415ee34607c6446dcc9a667ebb493e936d5a2a4
HarshJha2804 <harshjha117@gmail.com>
2025-10-24 01:55:11 +0530

    made changes

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.iunite.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.iunite.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.iunite_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.iunite_subnet[*].id
 }
